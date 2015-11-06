d99b9cb7fa0cf8c29492c1fbca0a55a45804675a
Harleen Kaur <iamharleen@gmail.com>
2015-11-06 13:02:28 -0800

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
